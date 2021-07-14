1dce80290aa69ee7121418d955ea52cf87ae6353
Anderson <sr.andersonmendesribeiro@gmail.com>
2021-07-14 20:58:34 -0300

    integration with mongoDB

---
+++ src/axios/index.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://catalog-api-b073ede8-e3f9-4d8a.herokuapp.com';
+
+export default axios;
--- src/components/Ad/Ad.js
+++ src/components/Ad/Ad.js
@@ -1,26 +1,8 @@
 import React from 'react';
 import './Ad.css';
-import PropTypes from 'prop-types';
 
-/*const Ad = () => (
-  <div>
-    Ad Component
-  </div>
-);
+export default function Ad ({url, description}) {
+  
+    return <img class='unique' src={url} alt={description} title={description}></img>
 
-Ad.propTypes = {};
-
-Ad.defaultProps = {};*/
-
-/*export default function Ad ({source, width, height}) {
-  return <img src={source + '/' + width + '/' + height} ></img> //<img src="{title}' + width + '/' + height + '" alt="pretty kitty"> </img>
-}*/
-
-export default function Ad ({source, source2}) {
-  console.log(source2);
-  if(source2) {
-    return <ul class='miniGalery'><img class='insideColumn' src={source} ></img><img class='insideColumn' src={source2} ></img></ul>
-  } else {
-    return <img class='unique' src={source} ></img>
-  }
 }
